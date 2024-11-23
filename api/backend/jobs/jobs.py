3ab49444d34a2d18e59665c0e39e1c0a0955561b
iasolb <ianspraguesolberg@gmail.com>
2024-11-22 19:57:47 -0500

    Deleted that, added experience file added 2 functions to experiences added create function to jobs

--- api/backend/jobs/jobs.py
+++ api/backend/jobs/jobs.py
@@ -7,3 +7,26 @@ from backend.db_connection import db
 
 jobs = Blueprint('jobs', __name__)
 
+# admin creates jobs
+@jobs.route('/jobs', methods=['POST'])
+def create_job():
+    current_app.logger.info('/jobs POST request')
+    job_info = request.get_json()
+    # init
+    EmpID = job_info['EmpID']
+    Title = job_info['Title']
+    Description = job_info['Description']
+    JobID = job_info['JobID']
+    data = (EmpID, Title, Description, JobID)
+    # query
+    query = '''
+    INSERT INTO jobs (EmpID, Title, Description, JobID)
+    VALUES (%s, %s, %s, %s)
+    '''
+    # cursor
+    cursor = db.get._db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+    return 'Job Created!'
+
+# admin deletes jobs
