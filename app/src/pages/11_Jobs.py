b188627c10f9a62e6b5ebe4ad0e0c8e41381071a
iasolb <ianspraguesolberg@gmail.com>
2024-11-24 16:28:14 -0500

    finally fixed database routing, rest entry and container names were inconsistent. Added routing some functionality to the Jobs page

--- app/src/pages/11_Jobs.py
+++ app/src/pages/11_Jobs.py
@@ -9,10 +9,11 @@ logger = logging.getLogger(__name__)
 st.set_page_config(layout='wide')
 
 # Display the appropriate sidebar links for the role of the logged-in user
-def SideBarLinks():
-    st.sidebar.header("")
-    st.sidebar.button("Home")
-    st.sidebar.button("Admin Job Editor")
+st.sidebar.header("Menu")
+if st.sidebar.button("Home"):
+    st.switch_page('Home.py')
+if st.sidebar.button("Back"):
+    st.switch_page('40_Admin_Home.py')
 
 SideBarLinks()
 
@@ -71,26 +72,3 @@ with col2:
             st.write("No jobs available.")
     else:
         st.error("Failed to fetch jobs.")
-
-'''
-# add one number input for variable 1 into column 1
-with col1:
-  var_01 = st.number_input('Variable 01:',
-                           step=1)
-
-# add another number input for variable 2 into column 2
-with col2:
-  var_02 = st.number_input('Variable 02:',
-                           step=1)
-
-logger.info(f'var_01 = {var_01}')
-logger.info(f'var_02 = {var_02}')
-
-# add a button to use the values entered into the number field to send to the 
-# prediction function via the REST API
-if st.button('Calculate Prediction',
-             type='primary',
-             use_container_width=True):
-  results = requests.get(f'http://api:4000/c/prediction/{var_01}/{var_02}').json()
-  st.dataframe(results)
-'''
